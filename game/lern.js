2f21139e2ea3d8af7522d086c3d0379824eeec29
shaxqq <39559342+shaxqq@users.noreply.github.com>
2018-06-04 16:08:20 +0300

    Update lern.js

--- game/lern.js
+++ game/lern.js
@@ -1,70 +1,3 @@
-// var numProg; // number
-//     numProg = Math.floor(Math.random() * 10) + 1;
-//     console.log(numProg);
-
-//     function show() {
-//         var num, out;
-//             num = document.getElementById('userNum').value;
-//             out = document.getElementById('result');
-
-//         if (num == numProg) {
-//             out.innerHTML = 'Вы угадали';
-//         }
-
-//         else if (num > numProg) {
-//             out.innerHTML = 'Число больше заданого';
-//         }
-//         else  
-//             out.innerHTML = 'Число меньше заданого';
-        
-//     }
-
-//     //massiv number
-//     function f1() { 
-//         var n = document.getElementById('n1');
-        
-//         for (var i=1; i<=100; i++) {
-//             n.innerHTML += i+' ';
-//         }
-//     }
-
-//     function f1() {
-//         var n = document.getElementById ('n1');
-
-//         var i=0;
-
-//         while (i<50) {
-//             i++;
-//             n.innerHTML += i+' ';
-//         }
-//     }
-
-
-//     var m1 = [5, 44, 'hi', 'yo']; //massive
-//         console.log (m1);
-
-//     function massOut() {
-//         var p = document.getElementById ('out');
-//         var str = '';
-//         for (var i=0; i<m1.length; i++) {
-//             str += i + ' --- ' + m1[i]+'<br>';
-//         }
-//         p.innerHTML = str;
-//     }
-// massOut();
-
-//     function p1() {
-//         var write = document.getElementById ('write').value;
-//         m1.push (write);
-//         massOut ();
-//     }
-
-//     function p2() {
-//         m1.pop (write);
-//         massOut ();
-//     }
-
-
 
 function send() {
     var mon1, clo, resultzp, resultprem, result, resultclock, resultnalog;
@@ -76,7 +9,7 @@ function send() {
 
         
         resultclock =  mon1*clo;
-        resultprem = resultclock * 0.175;
+        resultprem = resultclock * 0.1882;
         result = resultclock + resultprem;
         resultnalog = result * 0.195;
         resultzp =  result - resultnalog;
